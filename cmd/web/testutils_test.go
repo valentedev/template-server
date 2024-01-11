28dda40330905d3fe5cfc08d1bda406a880924dd
Rodrigo Valente <valentedev.rodrigo@gmail.com>
2024-01-11 10:28:01 -0300

    test userSignup

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -90,3 +90,19 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 
 	return rs.StatusCode, rs.Header, string(body)
 }
+
+// func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+// 	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	defer rs.Body.Close()
+// 	body, err := io.ReadAll(rs.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	body = bytes.TrimSpace(body)
+
+// 	return rs.StatusCode, rs.Header, string(body)
+// }
